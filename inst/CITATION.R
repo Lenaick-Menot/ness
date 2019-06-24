e159499e8b3fc063c4bcb9ed5f16d1bf41049171
Lenaick-Menot <52006349+Lenaick-Menot@users.noreply.github.com>
2019-06-24 17:12:23 +0200

    Add files via upload

---
+++ inst/CITATION.R
@@ -0,0 +1,15 @@
+citHeader("To cite ness in publications use:")
+
+citEntry(entry = "misc",
+         title        = "ness with R - hypergeometric standardization and probabilistic measures of similarity for community ecology",
+         author       = personList(as.person("Lénaïck Menot")),
+         journal      = "Zenodo",
+         year         = "2019",
+         url          = "https://doi.org/10.5281/zenodo.3254750",
+
+         textVersion  =
+           paste("Menot L. (2019).",
+                 "ness with R - hypergeometric standardization and probabilistic measures of similarity for community ecology - v0.1.0.",
+                 "Zenodo.",
+                 "https://doi.org/10.5281/zenodo.3254750")
+)
